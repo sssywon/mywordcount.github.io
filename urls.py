678997abf991934e8713ee08598bfcf4440bd734
sssywon <48916015+sssywon@users.noreply.github.com>
2019-06-11 17:42:24 +0900

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+import wordcount.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', wordcount.views.index, name="index"),
+    path('about/', wordcount.views.about, name="about"),
+    path('result/', wordcount.views.result, name="result"),
+]
